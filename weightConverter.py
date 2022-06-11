6e8ed615dbdf774d1bdacc34c38a4e79f36e541a
EricJBeck <105833761+EricJBeck@users.noreply.github.com>
2022-06-11 16:32:31 -0700

    Update weightConverter.py

--- weightConverter.py
+++ weightConverter.py
@@ -1,8 +1,8 @@
 weight = int(input('Weight: '))
 unit = input('(G)allons or (L)iters: ')
 if unit.upper() == "G":
-    converted = weight * 3.79
+    converted = weight / 0.26417
     print(f'You are {converted} Liters')
 else:
-    converted = weight / 3.79
+    converted = weight * 0.26417
     print(f'You are {converted} Gallons')
